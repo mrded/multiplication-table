66b1cf3d6c2366e5ec169c1d987834bf85962df0
Dmitry Demenchuk <dmitry@demenchuk.me>
2013-11-21 19:23:27 +0000

    Add test.

---
+++ spec/generator_spec.rb
@@ -0,0 +1,10 @@
+#encoding UTF-8
+require 'generator'
+
+describe Generator do
+
+  it "Returns an array of primes." do
+    Generator.new(10).generate.should eq [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+  end
+
+end
