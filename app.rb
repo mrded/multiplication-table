aee8b854e6218bcb6063c19863c191d2f400698b
Dmitry Demenchuk <dmitry@demenchuk.me>
2013-11-21 21:27:59 +0000

    Make table method.

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ Dir["./lib/*.rb"].each {|file| require file}
 # Get argument.
 count = (ARGV.first.to_i > 0) ? ARGV.first.to_i : 10
 
-prime = Generator.new(count).generate
-matrix = Calculator.new(prime).multiply
+primes = Generator.new(count).generate
+matrix = Calculator.new(primes).multiply
 
-Printer.new(matrix).table
+Printer.new(matrix: matrix, primes: primes).table
--- lib/printer.rb
+++ lib/printer.rb
@@ -1,15 +1,24 @@
 #encoding UTF-8
 
+class String
+  def colorize
+    "\033[44m#{self}\033[0m"
+  end
+end
+
 class Printer
   attr_accessor :matrix
 
-  def initialize(matrix = [])
-    @matrix = matrix
+  def initialize(options = {})
+    @matrix = options[:matrix]
+    @primes = options[:primes]
   end
 
   def table
-    @matrix.each do |row|
-      p row
+    puts "\t#{@primes.join("\t")}".colorize
+
+    @matrix.to_a.each_with_index do |row, key|
+      puts "#{@primes[key]}\t".colorize + "#{row.join("\t")}"
     end
   end
 end
